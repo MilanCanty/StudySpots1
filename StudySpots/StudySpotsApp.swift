31039ae4f682a3b537885eefc0aa0786b57dab3e
MilanCanty <milancanty@gmail.com>
2023-04-22 14:03:34 -0400

    Initial Commit

---
+++ StudySpots/StudySpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySpotsApp.swift
+//  StudySpots
+//
+//  Created by Milan Canty on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct StudySpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
